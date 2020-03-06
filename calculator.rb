874c74dad52a0b6183c64a07f93376743fbe32d7
Eric-Conde <37228372+Eric-Conde@users.noreply.github.com>
2020-03-06 19:54:34 -0300

    Add files via upload

---
+++ calculator.rb
@@ -0,0 +1,57 @@
+# # puts 'Digite o primeiro Número: '
+# # number1 = STDIN.gets
+# #
+# # puts 'Digite o segundo Número: '
+# # number2 = STDIN.gets
+# #
+# # puts 'Escolha uma operação'
+# # puts 'Adição = +'
+# # adição = STDIN.gets
+# # total = number1 + number2
+# # puts total
+# # puts 'Subtração = -'
+# # puts 'Divisão = /'
+# # puts 'Multiplicação = *'
+# #
+# #
+# #
+# #
+# #
+# #
+# # operation_typed = STDIN.gets
+# # total = STDIN.gets
+# #
+# # if operation_typed == +
+# #   total = number1 + number2
+#
+# $number1.to_i
+# $number2.to_i
+# $operation_typed.to_i
+#
+#
+# puts 'Digite o primeiro Número: '
+# number1 = STDIN.gets
+#
+# puts 'Digite o segundo Número: '
+# number2 = STDIN.gets
+#
+# puts 'Escolha uma operação:'
+#
+#     puts 'Adição = +'
+# 		puts 'Subtração = -'
+# 		puts 'Divisão = /'
+# 		puts 'Multiplicação = *'
+# 		puts 'Porcentagem = %'
+# 		puts 'Potencia = **'
+# 		puts 'Raiz = r'
+# 		puts 'Bhaskara = b'
+#
+#
+# case $operation_typed
+#
+# when $operation_typed = '+'
+#   puts 'number1 + number2'
+# end
+number1=1
+number2=2
+puts 'number1 + number2'
